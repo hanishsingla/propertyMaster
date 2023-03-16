cc1a2a92c812b3f021461175f048a8e6458e00b1
Anchal Singla <invoice.developer.02@gmail.com>
2023-03-16 12:44:23 +0530

    Menu setup

---
+++ src/Menu/PropertyHeaderMenu.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class PropertyHeaderMenu
+{
+
+
+    public function __construct(private readonly FactoryInterface $factory, private readonly RequestStack $requestStack, private readonly TranslatorInterface $translator)
+    {
+
+    }
+
+    public function createMainMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('root');
+        $trans = $this->translator;
+        $request = $this->requestStack;
+
+        $menu->setChildrenAttribute('class', 'navbar-nav  mb-2 mb-lg-0');
+        $currentRoute = $options['current_route'] ?? '';
+
+        $menu->addChild('Home', [
+            'route' => 'app_index',
+            'attribute' => 'nav-link',
+            'label' => 'Home',
+            'attributes' => ['class' => 'nav-item'],
+        ])->setLinkAttribute('class', 'nav-link');
+
+        $menu->addChild('Property', [
+            'route' => 'app_property',
+            'routeParameters' => ['dashboardType' => 'property'],
+            'attributes' => ['class' => 'nav-item'],
+        ])->setLinkAttribute('class', 'nav-link');
+
+        $menu->addChild('Agent', [
+            'route' => 'app_property',
+            'routeParameters' => ['dashboardType' => 'agent'],
+            'attributes' => ['class' => 'nav-item'],
+        ])->setLinkAttribute('class', 'nav-link');
+
+        // Set the current menu item as active
+        if ($currentRoute === 'app_index') {
+            $menu->getChild('Home')->setCurrent(true);
+        } elseif ($currentRoute === 'app_property') {
+            $menu->getChild('Property')->setCurrent(true);
+            $menu->getChild('Agent')->setCurrent(true);
+        }
+
+        return $menu;
+    }
+}
