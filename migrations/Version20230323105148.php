9ea96a5d297273af7f1a3aec45a64ffeecf33854
Abhinav Kumar <92725481+priyanka-verma96@users.noreply.github.com>
2023-03-23 18:01:08 +0530

    Update migration

--- migrations/Version20230323105148.php
+++ migrations/Version20230323105148.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230321072702 extends AbstractMigration
+final class Version20230323105148 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,7 +22,8 @@ final class Version20230321072702 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE agent (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', agent_number VARCHAR(255) DEFAULT NULL, agent_name VARCHAR(255) NOT NULL, is_deleted TINYINT(1) DEFAULT 0 NOT NULL, is_created_at DATETIME NOT NULL, is_updated_at DATETIME DEFAULT NULL, is_deleted_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', is_deleted TINYINT(1) DEFAULT 0 NOT NULL, is_created_at DATETIME NOT NULL, is_updated_at DATETIME DEFAULT NULL, is_deleted_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE property (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', property_title VARCHAR(255) NOT NULL, property_price VARCHAR(255) NOT NULL, property_area VARCHAR(255) NOT NULL, property_address VARCHAR(255) NOT NULL, short_description VARCHAR(255) NOT NULL, property_description VARCHAR(255) NOT NULL, is_deleted TINYINT(1) DEFAULT 0 NOT NULL, is_created_at DATETIME NOT NULL, is_updated_at DATETIME DEFAULT NULL, is_deleted_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE helpdesk (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, message VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1D6CD3B1E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE property (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', owner_id VARCHAR(255) NOT NULL, property_address VARCHAR(255) NOT NULL, property_area VARCHAR(255) NOT NULL, property_description VARCHAR(255) NOT NULL, property_image VARCHAR(255) DEFAULT NULL, property_price VARCHAR(255) NOT NULL, property_rooms VARCHAR(255) NOT NULL, property_status VARCHAR(255) NOT NULL, property_title VARCHAR(255) NOT NULL, property_type VARCHAR(255) NOT NULL, short_description VARCHAR(255) NOT NULL, is_deleted TINYINT(1) DEFAULT 0 NOT NULL, is_created_at DATETIME NOT NULL, is_updated_at DATETIME DEFAULT NULL, is_deleted_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE reset_password (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', user_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_B9983CE5A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE security_login (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', username VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, is_deleted TINYINT(1) DEFAULT 0 NOT NULL, is_created_at DATETIME NOT NULL, is_updated_at DATETIME DEFAULT NULL, is_deleted_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_46B428BAE7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -36,6 +37,7 @@ final class Version20230321072702 extends AbstractMigration
         $this->addSql('ALTER TABLE reset_password DROP FOREIGN KEY FK_B9983CE5A76ED395');
         $this->addSql('DROP TABLE agent');
         $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE helpdesk');
         $this->addSql('DROP TABLE property');
         $this->addSql('DROP TABLE reset_password');
         $this->addSql('DROP TABLE security_login');
