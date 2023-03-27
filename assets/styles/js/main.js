1e99604942f2d90ff6ced039b96d9abcb21e0d82
Abhinav Kumar <92725481+priyanka-verma96@users.noreply.github.com>
2023-03-27 15:48:49 +0530

    Update main js
    [data-popup="model"]  Not use

--- assets/styles/js/main.js
+++ assets/styles/js/main.js
@@ -1,45 +1,6 @@
 import bootbox from 'bootbox';
-
+import './slider';
 const body = 'body';
-$(body).on('click', '[data-popup="model"]', function (e) {
-    e.preventDefault();
-    const $panelbtn = $(this);
-    const url = $panelbtn.attr('href');
-    const title = $panelbtn.data('title');
-    const size = $panelbtn.data('size');
-    const id = $panelbtn.data('id');
-
-    $.ajax({
-        type: 'GET',
-        url: url,
-        success: function (response) {
-            bootbox.dialog({
-                title: title,
-                size: size || 'extra-large',
-                centerVertical: true,
-                message: response,
-            })
-            $(document).ready(function () {
-                $('#' + id).on('submit', function (e) {
-                    e.preventDefault();
-                    $.ajax({
-                        type: 'post',
-                        url: url,
-                        async: true,
-                        data: new FormData(this),
-                        contentType: false,
-                        cache: false,
-                        processData: false,
-                        dataType: 'JSON',
-                    })
-                    location.reload()
-                })
-            })
-
-        }
-
-    })
-})
 
 $(body).on('click', '[data-popup="submit"]', function (e) {
     e.preventDefault();
@@ -89,4 +50,8 @@ $(body).on('click', '[data-popup="submit"]', function (e) {
 
     })
 })
+
+
+
+
 
