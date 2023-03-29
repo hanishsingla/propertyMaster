bf7068dad108c01b41546a0776f9d0501cc13b60
Abhinav Kumar <invoice.developer.01@gmail.com>
2023-03-29 17:45:40 +0530

    layout update

---
+++ migrations/Version20230329102523.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230329102523 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE property CHANGE property_advance property_is_garage TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE property CHANGE property_is_garage property_advance TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+}
