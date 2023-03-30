71d6e7b85b59cb170eb573df14a2d38af8bd953f
Abhinav Kumar <invoice.developer.01@gmail.com>
2023-03-30 13:18:09 +0530

    Update Common Helper

--- src/Service/CommonHelper.php
+++ src/Service/CommonHelper.php
@@ -6,17 +6,9 @@ use DateTime;
 
 class CommonHelper
 {
-    public function setInformation($form, $ownerId): void
-    {
-        $information = $form->getData();
-        $information->setOwnerId($ownerId);
-        $this->setCreatedDate($information);
-    }
-
     public function setCreatedDate($information): void
     {
         $date = new DateTime;
         $information->setIsCreatedAt($date);
     }
-
 }
