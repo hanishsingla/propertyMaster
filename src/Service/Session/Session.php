1caa09be0833a4fccb52cfadd97c79f636c5c0a9
Abhinav Kumar <invoice.developer.01@gmail.com>
2023-04-03 17:26:17 +0530

    Session Update

--- src/Service/Session/Session.php
+++ src/Service/Session/Session.php
@@ -7,7 +7,7 @@ class Session
     public function session($user, $request): void
     {
         $request->getSession()->set('email'    , $user->getEmail());
-        $request->getSession()->set('ownerId'  , $user->getOwnerId());
+        $request->getSession()->set('ownerId'  , $user->getId());
         $request->getSession()->set('gender'  , $user->getUserinformation()->getGender());
         $request->getSession()->set('userImage'  , $user->getUserinformation()->getImage());
         $request->getSession()->set('userName' , $user->getUserinformation()->getName());
