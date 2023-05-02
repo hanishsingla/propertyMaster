9d165b22d55cab0e950f7b4f2d34ad753378ccd7
Abhinav Kumar <invoice.developer.01@gmail.com>
2023-05-02 16:43:42 +0530

    Default value selected

--- src/Form/Property/PropertyType.php
+++ src/Form/Property/PropertyType.php
@@ -139,13 +139,13 @@ class PropertyType extends AbstractType
 
             ->add('propertyStatus', ChoiceType::class, [
                 'choices' => [
-                    'Rent' => 'rent',
                     'Sell' => 'sell',
+                    'Rent' => 'rent',
                 ],
                 'choice_attr' => function() {
                     return ['class' => 'mx-2'];
                 },
-
+                'data' => 'sell',
                 'expanded' => true,
                 'multiple' => false,
             ])
