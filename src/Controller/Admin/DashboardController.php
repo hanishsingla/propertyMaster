84aa1ab0ba9533a3f86b656e375ac17618d81cb4
Anchal Singla <invoice.developer.02@gmail.com>
2023-05-04 13:24:26 +0530

    Update

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,14 +8,15 @@ use App\Entity\Property\Property;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[IsGranted('ROLE_ADMIN')]
     #[Route('/admin', name: 'admin')]
+    #[Security("is_granted('ROLE_ADMIN')")]
     public function index(): Response
     {
 
