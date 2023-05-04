053e092236319a6ec3c3143bb055e2bd081b19da
Anchal Singla <invoice.developer.02@gmail.com>
2023-05-04 13:25:07 +0530

    Entry Point add

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('js/app', './assets/app.js')
+    .addEntry('main/app', './assets/styles/js/main.js')
 
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
