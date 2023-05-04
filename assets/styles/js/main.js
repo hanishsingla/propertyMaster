f1b5952f3220e000de7ff695b141d5b2165521ad
Anchal Singla <invoice.developer.02@gmail.com>
2023-05-04 10:37:24 +0530

    Slick carousel

--- assets/styles/js/main.js
+++ assets/styles/js/main.js
@@ -93,7 +93,40 @@ $('.property-carousel').slick({
     infinite: true,
     speed: 500,
     fade: true,
-    cssEase: 'linear',
+    centerMode: true,
+    slidesToShow: 3,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    centerPadding: '60px',
+    // cssEase: 'linear',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
 
 /*==========  Favourite property  js start ===========*/
